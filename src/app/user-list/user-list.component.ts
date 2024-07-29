fdd7bd71cff8799a1a2b073eb3c808be313438ae
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-07-29 18:17:36 +0530

    build user list ts

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { UserDataService, User } from '../Services/User_data';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-list.component.html',
-  styleUrl: './user-list.component.css'
+  styleUrl: './user-list.component.css',
 })
 export class UserListComponent {
+  users: User[] = [];
+  filteredUsers: User[] = [];
+  pagedUsers: User[] = [];
+  workoutTypes = ['Running', 'Cycling', 'Swimming', 'Yoga'];
+  currentPage = 0;
+  pageSize = 5;
+  pageCount = 0;
 
+  constructor(private UserDataService: UserDataService) {
+    this.initializeData();
+  }
+
+  initializeData() {
+    this.users = this.UserDataService.getUsers();
+    this.filteredUsers = [...this.users];
+  }
 }
