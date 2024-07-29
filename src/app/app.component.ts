88a5b23d0c79bb8a354a6e6ca663a9df683a6efb
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-07-29 16:45:20 +0530

    import add-user component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AddUserComponent } from './add-user/add-user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AddUserComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Health_Challenger_Tracker';
