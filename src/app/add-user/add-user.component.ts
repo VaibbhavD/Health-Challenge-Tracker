d2a549178debc6aef616ce857e77961663bf5939
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-07-29 13:55:14 +0530

    add form build with onSubmit function

--- src/app/add-user/add-user.component.ts
+++ src/app/add-user/add-user.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-add-user',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, CommonModule],
   templateUrl: './add-user.component.html',
-  styleUrl: './add-user.component.css'
+  styleUrl: './add-user.component.css',
 })
 export class AddUserComponent {
+  workoutTypes = ['Running', 'Cycling', 'Swimming', 'Yoga'];
+  Name: string = '';
+  Worktype: string = '';
+  Minutes: number = 0;
 
+  onSubmit() {
+    let data = {
+      Name: this.Name,
+      Worktype: this.Worktype,
+      Minutes: this.Minutes,
+    };
+    console.log(data);
+  }
 }
