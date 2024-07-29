c133818c5061a35356ce1937fa981528cacd6e14
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-07-29 12:06:13 +0530

    create components and made folder structuer

---
+++ src/app/workout-charts/workout-charts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-workout-charts',
+  standalone: true,
+  imports: [],
+  templateUrl: './workout-charts.component.html',
+  styleUrl: './workout-charts.component.css'
+})
+export class WorkoutChartsComponent {
+
+}
