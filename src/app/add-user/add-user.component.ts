1dc6ab3f84cf9e327468b479450280f0159584b4
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-07-29 16:45:36 +0530

    clean code

--- src/app/add-user/add-user.component.ts
+++ src/app/add-user/add-user.component.ts
@@ -2,26 +2,34 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-
+import { UserDataService, User } from '../Services/User_data';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-add-user',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, CommonModule],
+  imports: [MatFormFieldModule, MatInputModule, CommonModule, FormsModule],
   templateUrl: './add-user.component.html',
   styleUrl: './add-user.component.css',
 })
 export class AddUserComponent {
   workoutTypes = ['Running', 'Cycling', 'Swimming', 'Yoga'];
-  Name: string = '';
-  Worktype: string = '';
-  Minutes: number = 0;
+
+  name: string = '';
+  workoutType: string = 'Cycling';
+  minutes: number = 0;
+
+  constructor(private UserDataService: UserDataService) {}
 
   onSubmit() {
-    let data = {
-      Name: this.Name,
-      Worktype: this.Worktype,
-      Minutes: this.Minutes,
+    let data: User = {
+      name: this.name,
+      workoutType: this.workoutType,
+      minutes: this.minutes,
     };
-    console.log(data);
+    this.UserDataService.addUser(data);
+    // Reset form fields if needed
+    this.name = '';
+    this.workoutType = '';
+    this.minutes = 0;
   }
 }
