147428651fca6381145bc014718ce0ad24e9f040
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-07-30 18:39:05 +0530

    update style and make responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AddUserComponent } from './add-user/add-user.component';
-
+import { UserListComponent } from './user-list/user-list.component';
+import { WorkoutChartComponent } from './workout-charts/workout-charts.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AddUserComponent],
+  imports: [
+    RouterOutlet,
+    AddUserComponent,
+    UserListComponent,
+    WorkoutChartComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
